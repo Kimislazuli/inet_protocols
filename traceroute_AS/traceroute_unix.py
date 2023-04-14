5f839a8caaa312e26e08fe3564cefee101f3ec55
Margarita Yarmolchuk <margaritayarmolchuk@gmail.com>
2023-04-14 23:24:28 +0500

    feat: complete parsing with regional registrar and ipinfo

--- traceroute_AS/traceroute_unix.py
+++ traceroute_AS/traceroute_unix.py
@@ -1,10 +1,12 @@
 """
-Unix script to parse traceroute and check AS for every router.
+Windows script to parse traceroute and check AS for every router.
 """
 import json
 import re
+import socket
 from subprocess import check_output
 from argparse import ArgumentParser
+from typing import Optional
 from urllib.request import urlopen
 
 from prettytable import PrettyTable
@@ -38,6 +40,121 @@ class Args:
 
         return args.destination
 
+def parse_json(url: str) -> Optional[dict[str]]:
+    """
+    Try open json and extract data.
+
+    :param url: link
+    :return: result or None if there's no result
+    """
+    try:
+        with urlopen(url) as handle:
+            return json.load(handle)
+    except AttributeError:
+        return None
+
+
+def regional_whois(ip_address: str, zone: str, regex: str) -> Optional[str]:
+    """
+    Ask specific regional whois.
+
+    :param ip_address: target address
+    :param zone: registrar
+    :param regex: regex for this registrar's whois response
+    :return: result or None if there's no result
+    """
+    # open socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('whois.' + zone + '.net', 43))
+    sock.sendall(f'{ip_address}\n'.encode('utf-8'))
+
+    response = b''
+
+    # load data
+    while True:
+        chunk = sock.recv(1024)
+        if not chunk:
+            break
+        response += chunk
+
+    # close socket
+    sock.close()
+
+    # check asn
+    as_number = re.findall(regex, response.decode('utf-8'))
+
+    if len(as_number):
+        return as_number[0]
+
+    return None
+
+
+def ask_regional_registrars(ip_address: str) -> Optional[str]:
+    """
+    Ask all regional registrars.
+
+    :param ip_address: target address
+    :return: result or None if there's no result
+    """
+    as_number = regional_whois(ip_address, 'ripe', r'origin:\s+AS(\d+)')
+
+    if as_number is None:
+        as_number = regional_whois(ip_address, 'lacnic', r'aut-num:\s+AS(\d+)')
+
+    if as_number is None:
+        as_number = regional_whois(ip_address, 'apnic', r'origin:\s+AS(\d+)')
+
+    if as_number is None:
+        as_number = regional_whois(ip_address, 'arin', r'OriginAS:\s+AS(\d+)')
+
+    if as_number is None:
+        as_number = regional_whois(ip_address, 'afrinic', r'(AS\d+)')
+
+    return as_number
+
+
+def ask_ip_info(ip_address: str) -> Optional[str]:
+    """
+    Ask ipinfo if regional registrars don't know.
+
+    :param ip_address: target address
+    :return: result or None if there's no result
+    """
+    data_from_json = parse_json('https://ipinfo.io/' + ip_address)
+
+    if data_from_json is None:
+        return None
+
+    try:
+        data = data_from_json['org']
+    except KeyError:
+        data = ''
+
+    as_number = re.findall(r'AS(\d+)', data)
+
+    if len(as_number):
+        return as_number[0]
+
+    return None
+
+
+def check_if_ip_is_grey(ip_address: str) -> bool:
+    """
+    Check if specific ip address is from grey range.
+
+    :param ip_address: target address
+    :return: bool: True if yes and False if no
+    """
+    for regex in GREY_NETWORKS_RANGE:
+        temp_match = re.match(regex, ip_address)
+
+        if temp_match is None:
+            continue
+
+        return True
+
+    return False
+
 
 def parse_info_by_ip(ip_address: str) -> list[str, str, str, str]:
     """
@@ -47,24 +164,28 @@ def parse_info_by_ip(ip_address: str) -> list[str, str, str, str]:
     :return: parsing results
     """
     # check is IP address grey
-    for regex in GREY_NETWORKS_RANGE:
-        temp_match = re.match(regex, ip_address)
-        if temp_match:
-            return [ip_address, 'grey ip address', '', '']
+    if check_if_ip_is_grey(ip_address):
+        return [ip_address, 'grey ip address', '', '']
 
     # parse json
-    url = 'https://api.incolumitas.com/?q=' + ip_address
+    data_from_json = parse_json('https://api.incolumitas.com/?q=' + ip_address)
 
-    with urlopen(url) as handle:
-        data_from_json = json.load(handle)
+    if data_from_json is not None:
+        asn = data_from_json['asn']
+    else:
+        asn = None
 
-    asn = data_from_json['asn']
-
+    # is there's no asn
     if isinstance(asn, dict):
         asn = asn['asn']
     elif asn is None:
-        asn = ''
+        asn = ask_regional_registrars(ip_address)
+        if asn is None:
+            asn = ask_ip_info(ip_address)
+            if asn is None:
+                asn = ''
 
+    # other data
     country = data_from_json['location']['country']
     city = data_from_json['location']['city']
 
@@ -122,7 +243,21 @@ def create_table(data: list) -> PrettyTable:
 def main():
     args = Args()
     output = check_output(['traceroute', args.destination]).splitlines()
-    print(create_table(output))
+
+    ptr = 0
+    while ptr < len(output):
+        line = output[ptr]
+        processing_result = process_output_line(line.decode('CP866'))
+
+        if len(processing_result) > 1:
+            destination = ', '.join(list(map(str, processing_result)))
+            print(f'Destination: {destination}')
+            ptr += 1
+            break
+
+        ptr += 1
+
+    print(create_table(output[ptr:]))
 
 
 if __name__ == '__main__':
